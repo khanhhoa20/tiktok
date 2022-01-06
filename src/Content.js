43daa77f4cdee93bec590f37c591188560f88a18
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-01-06 13:43:24 +0700

    useEffect with deps

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
+const tabs = ['posts', 'comments', 'albums']
 function Content() {
+    const [title, setTitle] = useState("");
+    const [posts, setPosts] = useState([]);
+    const [type, setType] = useState('posts')
+    // console.log(type);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then((res) => res.json())
+            .then((posts) => setPosts(posts));
+    }, [type]);
     return (
-        <h1>Hello World</h1>
-    )
+        <div>
+            {tabs.map(tab => (
+                <button
+                    key={tab}
+                    style={type === tab ? {
+                        color: '#fff',
+                        backgroundColor: '#333'
+                    } : {}}
+                    onClick={() => setType(tab)}>
+                    {tab}
+                </button>
+            ))}
+            <input value={title} onChange={(e) => setTitle(e.target.value)} />
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.title || post.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-export default Content
+export default Content;
