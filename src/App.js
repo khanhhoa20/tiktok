32c85add360e629d73916adadb6ce9be0cb57aaa
khanhhoa20 <khanhhoa12t2@gmail.com>
2022-01-20 18:29:38 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { useState } from 'react';
-import Content from './Content';
+
 
 function App() {
 
-  const [show, setShow] = useState(false)
+
 
   return (
-    <div className="App" style={{ padding: 32 }}>
-      <button onClick={() => {
-        setShow(!show)
-      }}>Toggle</button>
-      {show && <Content />}
+    <div className="App">
+
     </div>
 
   )
